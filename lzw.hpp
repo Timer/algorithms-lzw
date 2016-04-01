2eaf08ede89bc061227e0d558723e7ddaa354e6c
Joe Haddad <jsh77@zips.uakron.edu>
2016-04-01 10:22:58 -0400

    Add documentation to lzw.hpp

--- lzw.hpp
+++ lzw.hpp
@@ -17,21 +17,29 @@ public:
   LZWCompress(int dictionary_base, bool variable_dictionary, int dictionary_bits) {
     this->variable_dictionary = variable_dictionary;
     this->dictionary_bits = dictionary_bits;
+    //Instantiate the base dictionary
     for (int i = 0; i < dictionary_base; ++i) dictionary[std::string(1, i)] = i;
     this->dictionary_size = dictionary_base;
   }
 
   void compress(InputBuffer &in, OutputBuffer &out) {
+    //Compute the upper limit of the dicitionary
     unsigned int dictionary_limit = std::pow(2, out.get_word_length());
     std::string word;
     for (;;) {
       try {
+        //Get what we're compressing next
         int value = in.read();
+        //Build prefix
         std::string prefix = word + std::string(1, value);
+        //If prefix exists, keep building ->
         if (dictionary.count(prefix)) {
           word = prefix;
-        } else {
+        } else
+        //Prefix doesn't exist, append prefix and start building new prefix ->
+        {
           out.write(dictionary[word]);
+          //Expand dictionary if need be ->
           if (dictionary_size < dictionary_limit || (variable_dictionary && out.get_word_length() < dictionary_bits)) {
             if (variable_dictionary && dictionary_size >= dictionary_limit) {
               out.set_word_length(out.get_word_length() + 1);
@@ -60,21 +68,28 @@ public:
   LZWInflate(int dictionary_base, bool variable_dictionary, int dictionary_bits) {
     this->variable_dictionary = variable_dictionary;
     this->dictionary_bits = dictionary_bits;
+    //Instantiate the base dictionary
     for (int i = 0; i < dictionary_base; ++i) dictionary[i] = std::string(1, i);
     this->dictionary_size = dictionary_base;
   }
 
   void decompress(InputBuffer &in, OutputBuffer &out) {
+    //Compute the upper limit of the dictionary
     unsigned int dictionary_limit = std::pow(2, in.get_word_length());
     std::string entry;
+    //Start reading file, base word is first entry
     std::string word(1, in.read());
+    //We know first byte isn't a code, so write it
     out.write(word[0]);
     for (;;) {
       try {
+        //Read next code word
         unsigned int value = in.read();
+        //Word exists, retrieve it ->
         if (dictionary.count(value)) {
           entry = dictionary[value];
         } else if (value == dictionary_size) {
+          //We need to create a new word ... build entry
           entry = word + word[0];
         } else {
           std::string msg("bad compression ... value encountered: ");
@@ -89,7 +104,9 @@ public:
           msg += std::to_string(in.get_word_length());
           throw std::runtime_error(msg);
         }
+        //Write information out to output buffer
         for (unsigned char c : entry) out.write(c);
+        //Increase dictionary size
         if (dictionary_size < dictionary_limit || (variable_dictionary && in.get_word_length() < dictionary_bits)) {
           if (variable_dictionary && dictionary_size >= dictionary_limit - 1 && in.get_word_length() < dictionary_bits) {
             in.set_word_length(in.get_word_length() + 1);
